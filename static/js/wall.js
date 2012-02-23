6a50b0514470ca4ccc71b902064a80ecf64d8252
John McLear <john@mclear.co.uk>
2012-02-23 10:58:17 +0000

    added functions for a random note color..

--- static/js/wall.js
+++ static/js/wall.js
@@ -1002,3 +1002,23 @@ function showAsReadOnly(){
   $('.noteeditoption').addClass("hidden");
   $('.notetitle').css({"cursor":"default"});
 }
+
+function randomNoteColor()
+{
+  var colors = ["#4CC2E6","#FDDF20","#C6DE6A","#F49BBA"];
+  randno = Math.floor ( Math.random() * colors.length );
+  return(colors[randno]);
+}
+
+var hexDigits = new Array ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
+
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+}
+
