8873777762829e56abf14fdcc4dea200c954f6bb
John McLear <john@mclear.co.uk>
2012-03-06 13:51:43 +0000

    Update primarywall.sql

--- primarywall.sql
+++ primarywall.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `note` (
   `x` int(4) NOT NULL COMMENT 'x coordinate of the note',
   `y` int(4) NOT NULL COMMENT 'y coordinate of the note',
   `wallid` varchar(80) NOT NULL COMMENT 'id of the wall this note exists on',
-  `color` varchar(1) NOT NULL COMMENT 'the color of the note',
+  `color` varchar(10) NOT NULL COMMENT 'the color of the note',
   PRIMARY KEY (`guid`),
   KEY `wallid` (`wallid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
