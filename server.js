91151ca366c7ca2e5c50052261117ca42dc86ff4
John McLear <john@mclear.co.uk>
2012-05-21 17:10:48 +0200

    Fix issue #18

--- server.js
+++ server.js
@@ -118,6 +118,11 @@ function requestLog(code, path, desc)
 
 //var io = io.listen(server);
 var io = require('socket.io').listen(server)
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set("polling duration", 10);            // allow for long polling
+io.set('log level', 1);                    // reduce logging
 
 var messageHandler = require("./MessageHandler");
 messageHandler.setSocketIO(io);
